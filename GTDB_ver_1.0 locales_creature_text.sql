22697e4bcd1670d3dbc06bbe1cb5d60759fd8794
unknown <info@xn--kfgames-5wa.de>
2013-10-22 22:53:37 +0200

    fixed .sql

--- GTDB_ver_1.0 locales_creature_text.sql
+++ GTDB_ver_1.0 locales_creature_text.sql
@@ -72,37 +72,37 @@ INSERT INTO `locales_creature_text` (`entry`, `groupid`, `id`, `text_loc1`, `tex
 	(2859, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(2861, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(2941, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (2991, 0, 0, '', '', '%s deutet auf den Wasserkrug am Rande des Brunnens.', '', '', '', '', '')
+	(2991, 0, 0, '', '', '%s deutet auf den Wasserkrug am Rande des Brunnens.', '', '', '', '', ''),
 	(2995, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (3301, 1, 0, '', '', 'Mein Schwert Archeus hat mir im Leben gute Dienste geleistet, da aber mein Geist in Bälde von diesem freudlosen Dasein scheiden wird, brauche ich es nicht länger.', '', '', '', '', '')
-    (3301, 2, 0, '', '', 'Ich sollte an der Liebe zu meiner Tochter festhalten und hoffen, dass ich unter dem Lichte meiner Sünden Vergebung finden werde.', '', '', '', '', '')
+	(3301, 1, 0, '', '', 'Mein Schwert Archeus hat mir im Leben gute Dienste geleistet, da aber mein Geist in Bälde von diesem freudlosen Dasein scheiden wird, brauche ich es nicht länger.', '', '', '', '', ''),
+	(3301, 2, 0, '', '', 'Ich sollte an der Liebe zu meiner Tochter festhalten und hoffen, dass ich unter dem Lichte meiner Sünden Vergebung finden werde.', '', '', '', '', ''),
 	(3305, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(3310, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (3389, 0, 0, '', '', 'Hütet euch, $N! Wendet den Blick nach Westen!', '', '', '', '', '')
-    (3389, 1, 0, '', '', 'Ein Verteidiger ist gefallen!', '', '', '', '', '')
-    (3584, 0, 0, '', '', 'Ok, Lasst uns gehen!', '', '', '', '', '')
-    (3584, 1, 0, '', '', 'Den Rest des Weges schaffe ich allein, $N. Habt Dank!', '', '', '', '', '')
+	(3389, 0, 0, '', '', 'Hütet euch, $N! Wendet den Blick nach Westen!', '', '', '', '', ''),
+	(3389, 1, 0, '', '', 'Ein Verteidiger ist gefallen!', '', '', '', '', ''),
+	(3584, 0, 0, '', '', 'Ok, Lasst uns gehen!', '', '', '', '', ''),
+	(3584, 1, 0, '', '', 'Den Rest des Weges schaffe ich allein, $N. Habt Dank!', '', '', '', '', ''),
 	(3615, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (3627, 0, 0, '', '', 'Magische Studien belasten euer Gehirn? Erholt euch beim Blauen Eremiten!', '', '', '', '', '')
-    (3627, 0, 1, '', '', 'Die besten Getränke in Sturmwind!', '', '', '', '', '')
-    (3627, 0, 2, '', '', 'Kommt ihr nur für das Bier, für die Atmosphäre bleibt ihr hier !', '', '', '', '', '')
-    (3627, 0, 3, '', '', 'Der einzige Ort in Sturmwind wo sich Magie und Geister verbinden. Kommt zum Blauen Eremiten!', '', '', '', '', '')
-    (3627, 0, 4, '', '', 'Freigetränke im Blauen Eremiten!', '', '', '', '', '')
-    (3627, 0, 5, '', '', 'Bist du traurig? Komm in den Blauen Eremiten und habe eine gute Zeit!', '', '', '', '', '')
-    (3627, 0, 6, '', '', 'Macht euch auf in den Blauen Eremiten. Wo jeder euren Namen kennt!', '', '', '', '', '')
-    (3692, 0, 0, '', '', 'Alles klar, lasst uns gehen.', '', '', '', '', '')
-    (3692, 1, 0, '', '', 'Oh nein! Sie sind hier!', '', '', '', '', '')
-    (3692, 2, 0, '', '', 'OK, $N. Von hier schaffe ich es allein nach Auberdine. Geht nach Terenthis, er wartet auf euch. Danke für die Hilfe!', '', '', '', '', '')
-    (3692, 3, 0, '', '', 'OK, viel Glück mein Freund. Danke für die Hilfe.', '', '', '', '', '')
-    (3693, 0, 0, '', '', 'Was geschieht hier?', '', '', '', '', '')
-    (3693, 1, 0, '', '', 'Volcor braucht Hilfe?', '', '', '', '', '')
-    (3693, 2, 0, '', '', 'Alles klar, ich sollte Leute finden, die ihm helfen.', '', '', '', '', '')
-    (3693, 3, 0, '', '', 'Hallo Grimclaw.', '', '', '', '', '')
-    (3693, 4, 0, '', '', 'Was geschieht hier?', '', '', '', '', '')
-    (3693, 5, 0, '', '', 'Volcor braucht Hilfe?', '', '', '', '', '')
-    (3693, 6, 0, '', '', 'Du weißt wo er ist?', '', '', '', '', '')
-    (3693, 7, 0, '', '', 'Nun, ok. Ich sende Leute. Wir müssen ihn finden und ihm helfen!', '', '', '', '', '')
-	(3695, 0, 0, '', '', 'Grimclaw knurrt dich an bevor er sich Zeit nimmt, dich zu beschnuppern.', '', '', '', '', '')
+	(3627, 0, 0, '', '', 'Magische Studien belasten euer Gehirn? Erholt euch beim Blauen Eremiten!', '', '', '', '', ''),
+	(3627, 0, 1, '', '', 'Die besten Getränke in Sturmwind!', '', '', '', '', ''),
+	(3627, 0, 2, '', '', 'Kommt ihr nur für das Bier, für die Atmosphäre bleibt ihr hier !', '', '', '', '', ''),
+	(3627, 0, 3, '', '', 'Der einzige Ort in Sturmwind wo sich Magie und Geister verbinden. Kommt zum Blauen Eremiten!', '', '', '', '', ''),
+	(3627, 0, 4, '', '', 'Freigetränke im Blauen Eremiten!', '', '', '', '', ''),
+	(3627, 0, 5, '', '', 'Bist du traurig? Komm in den Blauen Eremiten und habe eine gute Zeit!', '', '', '', '', ''),
+	(3627, 0, 6, '', '', 'Macht euch auf in den Blauen Eremiten. Wo jeder euren Namen kennt!', '', '', '', '', ''),
+	(3692, 0, 0, '', '', 'Alles klar, lasst uns gehen.', '', '', '', '', ''),
+	(3692, 1, 0, '', '', 'Oh nein! Sie sind hier!', '', '', '', '', ''),
+	(3692, 2, 0, '', '', 'OK, $N. Von hier schaffe ich es allein nach Auberdine. Geht nach Terenthis, er wartet auf euch. Danke für die Hilfe!', '', '', '', '', ''),
+	(3692, 3, 0, '', '', 'OK, viel Glück mein Freund. Danke für die Hilfe.', '', '', '', '', ''),
+	(3693, 0, 0, '', '', 'Was geschieht hier?', '', '', '', '', ''),
+	(3693, 1, 0, '', '', 'Volcor braucht Hilfe?', '', '', '', '', ''),
+	(3693, 2, 0, '', '', 'Alles klar, ich sollte Leute finden, die ihm helfen.', '', '', '', '', ''),
+	(3693, 3, 0, '', '', 'Hallo Grimclaw.', '', '', '', '', ''),
+	(3693, 4, 0, '', '', 'Was geschieht hier?', '', '', '', '', ''),
+	(3693, 5, 0, '', '', 'Volcor braucht Hilfe?', '', '', '', '', ''),
+	(3693, 6, 0, '', '', 'Du weißt wo er ist?', '', '', '', '', ''),
+	(3693, 7, 0, '', '', 'Nun, ok. Ich sende Leute. Wir müssen ihn finden und ihm helfen!', '', '', '', '', ''),
+	(3695, 0, 0, '', '', 'Grimclaw knurrt dich an bevor er sich Zeit nimmt, dich zu beschnuppern.', '', '', '', '', ''),
 	(3838, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(3841, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(4267, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
@@ -111,44 +111,44 @@ INSERT INTO `locales_creature_text` (`entry`, `groupid`, `id`, `text_loc1`, `tex
 	(4317, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(4319, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(4321, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (4351, 0, 0, '', '', '%s sieht schwach genug zum Fangen aus.', '', '', '', '', '')
-    (4352, 0, 0, '', '', '%s sieht schwach genug zum Fangen aus.', '', '', '', '', '')
+	(4351, 0, 0, '', '', '%s sieht schwach genug zum Fangen aus.', '', '', '', '', ''),
+	(4352, 0, 0, '', '', '%s sieht schwach genug zum Fangen aus.', '', '', '', '', ''),
 	(4407, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(4551, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (4961, 0, 0, '', '', 'Jetzt werdet ihr schon alles zum Guten wenden!', '', '', '', '', '')
-    (4961, 1, 0, '', '', 'Okay, okay! Genug des Kampfes. Es braucht niemand mehr verletzt werden.', '', '', '', '', '')
-    (4961, 2, 0, '', '', 'Es ist in Ordnung, Jungs. Verschwindet. Ihr habt genug getan. Ich treffe mich später mit euch.', '', '', '', '', '')
+	(4961, 0, 0, '', '', 'Jetzt werdet ihr schon alles zum Guten wenden!', '', '', '', '', ''),
+	(4961, 1, 0, '', '', 'Okay, okay! Genug des Kampfes. Es braucht niemand mehr verletzt werden.', '', '', '', '', ''),
+	(4961, 2, 0, '', '', 'Es ist in Ordnung, Jungs. Verschwindet. Ihr habt genug getan. Ich treffe mich später mit euch.', '', '', '', '', ''),
 	(6026, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (6172, 0, 0, '', '', 'Habt Dank, lieber $C, Ihr habt gerade mein Leben gerettet.', '', '', '', '', '')
-    (6176, 0, 0, '', '', 'Folgt mir, $N. Ich werde schon bald mit der Beschwörung beginnen.', '', '', '', '', '')
-    (6176, 1, 0, '', '', '%s beginnt mit einem Beschwörungszauber...', '', '', '', '', '')
-    (6177, 0, 0, '', '', 'Habt Dank, lieber $C, Ihr habt gerade mein Leben gerettet.', '', '', '', '', '')
-    (6546, 0, 0, '', '', 'Bewaffnet euch, $N. Der Dämon in diesem Orb ist eine gemeine Bestie.', '', '', '', '', '')
+	(6172, 0, 0, '', '', 'Habt Dank, lieber $C, Ihr habt gerade mein Leben gerettet.', '', '', '', '', ''),
+	(6176, 0, 0, '', '', 'Folgt mir, $N. Ich werde schon bald mit der Beschwörung beginnen.', '', '', '', '', ''),
+	(6176, 1, 0, '', '', '%s beginnt mit einem Beschwörungszauber...', '', '', '', '', ''),
+	(6177, 0, 0, '', '', 'Habt Dank, lieber $C, Ihr habt gerade mein Leben gerettet.', '', '', '', '', ''),
+	(6546, 0, 0, '', '', 'Bewaffnet euch, $N. Der Dämon in diesem Orb ist eine gemeine Bestie.', '', '', '', '', ''),
 	(6706, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(6726, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (7207, 0, 0, '', '', 'Das ist interessant...', '', '', '', '', '')
-    (7207, 1, 0, '', '', 'Ich muss Noram und Horatio konsultieren.', '', '', '', '', '')
-    (7604, 0, 0, '', '', 'Was?  Wie könnt ihr es wagen, so etwas zu mir zu sagen?!?', '', '', '', '', '')
-    (7604, 1, 0, '', '', 'Nach allem, was wir durchgemacht haben?  Nun, ich mochte dich sowieso nicht!!', '', '', '', '', '')
-    (7804, 0, 0, '', '', 'Hört her, Bewohner von Tanaris! Lasst verkünden, dass $N ein bedeutendes Mitglied des Mithril Ordens. Ein Schmied der Ehre, Hingabe, und unendlichen Geduld. Ein dreifaches Hoch auf $N!', '', '', '', '', '')
+	(7207, 0, 0, '', '', 'Das ist interessant...', '', '', '', '', ''),
+	(7207, 1, 0, '', '', 'Ich muss Noram und Horatio konsultieren.', '', '', '', '', ''),
+	(7604, 0, 0, '', '', 'Was?  Wie könnt ihr es wagen, so etwas zu mir zu sagen?!?', '', '', '', '', ''),
+	(7604, 1, 0, '', '', 'Nach allem, was wir durchgemacht haben?  Nun, ich mochte dich sowieso nicht!!', '', '', '', '', ''),
+	(7804, 0, 0, '', '', 'Hört her, Bewohner von Tanaris! Lasst verkünden, dass $N ein bedeutendes Mitglied des Mithril Ordens. Ein Schmied der Ehre, Hingabe, und unendlichen Geduld. Ein dreifaches Hoch auf $N!', '', '', '', '', ''),
 	(7823, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(7824, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(8018, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(8019, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(8020, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-	(8479, 0, 0, '', '', 'Habt Geduld, $N. Die Fackel ist so gut wie fertig.', '', '', '', '', '')
+	(8479, 0, 0, '', '', 'Habt Geduld, $N. Die Fackel ist so gut wie fertig.', '', '', '', '', ''),
 	(8609, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(8610, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
-    (9117, 0, 0, '', '', 'Das macht Sinn!', '', '', '', '', '')
-    (9456, 0, 0, '', '', 'Die Kolkar sind die Stärksten!', '', '', '', '', '')
-    (9458, 0, 0, '', '', 'Verteidigt die Bunker!', '', '', '', '', '')
-    (9458, 0, 1, '', '', 'Unsere Gegner werden versagen!', '', '', '', '', '')
-    (9458, 0, 2, '', '', 'Für die Horde', '', '', '', '', '')
-    (9461, 0, 0, '', '', '%s verfällt in eine Raserei!', '', '', '', '', '')
-    (9524, 0, 0, '', '', 'Eindringling der Kolkar zielt!', '', '', '', '', '')
-    (9546, 0, 0, '', '', 'Oh man, Ich dachte schon das sei mein sicheres Ende. Ugh... bin immernoch benebelt...', '', '', '', '', '')
-    (9546, 1, 0, '', '', 'Ich denke ich schaffe es alleine zurück zur Festung. Nun, da ihr mir etwas Zeit gegeben habt, sollte ich in der Lage sein, mich von hier fortzuschleichen. Wer auch immer ihr seid... danke. Möge Elune euch ewig segnen!', '', '', '', '', '')
-    (9564, 0, 0, '', '', 'Der Zeppelin nach Unterstadt ist gerade eingetroffen! Alles einsteigen nach Tirisfal!', '', '', '', '', '')
+	(9117, 0, 0, '', '', 'Das macht Sinn!', '', '', '', '', ''),
+	(9456, 0, 0, '', '', 'Die Kolkar sind die Stärksten!', '', '', '', '', ''),
+	(9458, 0, 0, '', '', 'Verteidigt die Bunker!', '', '', '', '', ''),
+	(9458, 0, 1, '', '', 'Unsere Gegner werden versagen!', '', '', '', '', ''),
+	(9458, 0, 2, '', '', 'Für die Horde', '', '', '', '', ''),
+	(9461, 0, 0, '', '', '%s verfällt in eine Raserei!', '', '', '', '', ''),
+	(9524, 0, 0, '', '', 'Eindringling der Kolkar zielt!', '', '', '', '', ''),
+	(9546, 0, 0, '', '', 'Oh man, Ich dachte schon das sei mein sicheres Ende. Ugh... bin immernoch benebelt...', '', '', '', '', ''),
+	(9546, 1, 0, '', '', 'Ich denke ich schaffe es alleine zurück zur Festung. Nun, da ihr mir etwas Zeit gegeben habt, sollte ich in der Lage sein, mich von hier fortzuschleichen. Wer auch immer ihr seid... danke. Möge Elune euch ewig segnen!', '', '', '', '', ''),
+	(9564, 0, 0, '', '', 'Der Zeppelin nach Unterstadt ist gerade eingetroffen! Alles einsteigen nach Tirisfal!', '', '', '', '', ''),
 	(10378, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(10897, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
 	(11138, 0, 0, '', '', 'Wachen!', '', '', '', '', ''),
