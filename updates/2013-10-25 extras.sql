a34c7f725315682ab919278980aefcb2e4999982
D3thw0lf <info@xn--kfgames-5wa.de>
2013-10-31 02:17:58 +0100

    fixed bad coding...sorry :/

--- updates/2013-10-25 extras.sql
+++ updates/2013-10-25 extras.sql
@@ -1,8 +1,8 @@
-SET NAMES 'utf8';
+ï»¿SET NAMES 'utf8';
 /* Nur benutzen, wenn du vor dem 25. Oktober 2013 die "GTDB_ver_1.0 extras.sql" bereits gebatcht hast! */
-UPDATE creature_ai_texts SET content_loc3='Völlig wirkungslos. So wie jemand anderes den Ich kenne.' WHERE entry=-74;
-UPDATE creature_ai_texts SET content_loc3='Füttert, mich bruddahs!' WHERE entry=-135; --klingt irgendwie verrückt...hat jemand eine bessere Idee für "Feed, me bruddahs!"?
-UPDATE creature_ai_texts SET content_loc3='Hör auf zu schreien!' WHERE entry=-136;
+UPDATE creature_ai_texts SET content_loc3='VÃ¶llig wirkungslos. So wie jemand anderes den Ich kenne.' WHERE entry=-74;
+UPDATE creature_ai_texts SET content_loc3='FÃ¼ttert, mich bruddahs!' WHERE entry=-135; --klingt irgendwie verrÃ¼ckt...hat jemand eine bessere Idee fÃ¼r "Feed, me bruddahs!"?
+UPDATE creature_ai_texts SET content_loc3='HÃ¶r auf zu schreien!' WHERE entry=-136;
 UPDATE creature_ai_texts SET content_loc3='Du hast nichts!' WHERE entry=-137;
 UPDATE creature_ai_texts SET content_loc3='Alles was du tust, verschwendet meine Zeit!' WHERE entry=-138;
 UPDATE creature_ai_texts SET content_loc3='Komm zu mir bruddah !' WHERE entry=-139;
