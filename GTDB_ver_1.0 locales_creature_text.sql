7e614763e59e46c515cf5954ca99b790654837aa
D3thw0lf <info@xn--kfgames-5wa.de>
2013-11-01 08:23:36 +0100

    fix for the sql batch error...sorry :/

--- GTDB_ver_1.0 locales_creature_text.sql
+++ GTDB_ver_1.0 locales_creature_text.sql
@@ -1,4 +1,4 @@
-#############################################################################
+﻿#############################################################################
 # Copyright (C) 2013 KaefGAMES <http://www.kaefgames.de>                  #
 #									    #
 # This program is free software: you can redistribute it and/or modify      #
