35344cb8080eb462e091cc89ef5a2315eeb92cb0
D3thw0lf <info@xn--kfgames-5wa.de>
2013-11-01 09:38:19 +0100

    removed bad comment..

--- updates/old/2013-10-25 extras.sql
+++ updates/old/2013-10-25 extras.sql
@@ -1,7 +1,7 @@
 ﻿SET NAMES 'utf8';
 /* Nur benutzen, wenn du vor dem 25. Oktober 2013 die "GTDB_ver_1.0 extras.sql" bereits gebatcht hast! */
 UPDATE creature_ai_texts SET content_loc3='Völlig wirkungslos. So wie jemand anderes den Ich kenne.' WHERE entry=-74;
-UPDATE creature_ai_texts SET content_loc3='Füttert, mich bruddahs!' WHERE entry=-135; --klingt irgendwie verrückt...hat jemand eine bessere Idee für "Feed, me bruddahs!"?
+UPDATE creature_ai_texts SET content_loc3='Füttert, mich bruddahs!' WHERE entry=-135;
 UPDATE creature_ai_texts SET content_loc3='Hör auf zu schreien!' WHERE entry=-136;
 UPDATE creature_ai_texts SET content_loc3='Du hast nichts!' WHERE entry=-137;
 UPDATE creature_ai_texts SET content_loc3='Alles was du tust, verschwendet meine Zeit!' WHERE entry=-138;
