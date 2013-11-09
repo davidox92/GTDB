b612081da6a44a92192eea8c78ddeb1844ff331c
D3thw0lf <info@xn--kfgames-5wa.de>
2013-11-09 11:12:26 +0100

    a fix from trinitycore...

---
+++ updates/2013-11-9 creature_ai_scripts_0.sql
@@ -0,0 +1,5 @@
+﻿SET NAMES 'utf8';
+-- Remove some EAI already converted to cpp - latest fix from trinitycore
+DELETE FROM creature_ai_scripts WHERE creature_id IN (25040,26499,30663,30918,32273);
+UPDATE creature_template SET AIName = '' WHERE entry IN (25040,26499,30663,30918,32273);
+SET NAMES 'latin1';
